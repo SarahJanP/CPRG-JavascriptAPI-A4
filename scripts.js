1ed5fc27cd84ed22ceb6dbedc14e44da28e7d8ab
SarahJanP <sarah.parker@edu.sait.ca>
2025-03-22 15:35:05 -0600

    Feat: Made each page print specifically if its a two part or single part joke and of what kind, including on load joke of day. AND Bugfix:  Used Javascript validator to clear some javscript errors (ex. defining consts multiple times)

--- scripts.js
+++ scripts.js
@@ -1,167 +1,171 @@
 // Sarah Parker | 000900287
-const pageTitle = document.getElementById("pageTitle");
-const jokeCategory = document .getElementById("jokeCategory")
-const jokeSection = document.getElementById("jokeSection");
 
 
-//WHEN THE PAGE FIRST LOADS,  DO THE JOKE OF THE DAY.
-window.onload = jokeOfDay();
 
-//Button Constants
-const randomPun = document.getElementById("buttonPun")
-const randomMisc = document.getElementById("buttonMisc")
-const randomSpooky = document.getElementById("buttonSpooky")
-const randomChristmas = document.getElementById("buttonChristmas")
-const randomProgram = document.getElementById("buttonProgram")
-
-//Event Listeners
-
-randomPun.addEventListener("click", getPun);
-randomMisc.addEventListener("click", getMisc);
-randomSpooky.addEventListener("click", getSpooky);
-randomChristmas.addEventListener("click", getChristmas);
-randomProgram.addEventListener("click", getProgram);
-
-        
 //FUNCTIONS
 
-function jokeOfDay(){
+
+function jokeOfDay() {
     fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
-    .then(response => response.json())
-    .then(data => {
+.then(response => response.json())
+.then(data => {
         console.log(data);
-        pageTitle.innerText = "Joke Of The Day!"
 
+        //IF ITS A TWO PART PUN SAY HERES A TWO PART PUN
+        if ((data.type == "twopart") && (data.category == "Pun") ){
+            jokeType.innerText = "Here's a Two-Part Pun:";
+            joke.innerText = data.setup + " ... " + data.delivery;
+        }
+        //IF ITS A SINGLE PART PUN SAY HERES A SINGLE PART PUN
+        else if ((data.type == "single") && (data.category == "Pun") ){
 
-        if (data.type == "twopart" ){
-            //Print the type of joke
-            jokeType.innerText = "Here's a two-part joke:";
-            joke.innerHTML = data.setup + " ... " + data.delivery;
+            jokeType.innerText = "Here's a Single-Part Pun:";
+            joke.innerText = data.joke;
+        }
+        
+        
+        else if ((data.type == "twopart")){
+
+            jokeType.innerText = "Here's A Two-Part " + data.category + " Joke:";
+            joke.innerText = data.setup + " ... " + data.delivery;
         }
 
         else{
-            jokeType.innerText = "Here's a single-part joke:";
+            jokeType.innerText = "Here's a Single-Part " + data.category + " Joke:";
             joke.innerText = data.joke;
         }
-    });
+    })
+    .catch(error => console.error(error));
 }
 
 
 
+
 function getPun() {
-    fetch("https://v2.jokeapi.dev/joke/Pun?safe-mode")  //Fetch a pun joke
+    fetch("https://v2.jokeapi.dev/joke/Pun?safe-mode")  //Fetch a pun
     .then(response => response.json())
     .then(data => {
-            console.log(data);
-            jokeType.innerText = "Joke type: " + data.type;
-
-
+        console.log(data);
+        jokeType.innerText = "Joke type: " + data.type;
 
         pageTitle.innerText = "A Random Pun"; 
 
         if (data.type == "twopart" ){
             //Print the type of joke
-            jokeType.innerText = "Here's a two-part pun:";
-            joke.innerHTML = data.setup + " ... " + data.delivery;
+            jokeType.innerText = "Here's a Two-Part Pun:";
+            joke.innerText = data.setup + " ... " + data.delivery;
         }
 
         else{
-            jokeType.innerText = "Here's a single-part pun:";
+            jokeType.innerText = "Here's a Single-Part Pun:";
             joke.innerText = data.joke;
         }
     })
+    .catch(error => console.error(error));
 }
 
 
 function getMisc() {
-    fetch("https://v2.jokeapi.dev/joke/Misc?safe-mode")  //Fetch a pun joke
+    fetch("https://v2.jokeapi.dev/joke/Misc?safe-mode")  //Fetch a Misc
     .then(response => response.json())
     .then(data => {
-            console.log(data);
-            jokeType.innerText = "Joke type: " + data.type;
+        console.log(data);
+        jokeType.innerText = "Joke type: " + data.type;
 
         pageTitle.innerText = "A Random Miscellaneous Joke"; 
 
         if (data.type == "twopart" ){
             //Print the type of joke
-            jokeType.innerText = "Here's a two-part miscellaneous joke:";
-            joke.innerHTML = data.setup + " ... " + data.delivery;
+            jokeType.innerText = "Here's a Two-Part Miscellaneous Joke:";
+            joke.innerText = data.setup + " ... " + data.delivery;
         }
 
         else{
-            jokeType.innerText = "Here's a single-part miscellaneous joke:";
+            jokeType.innerText = "Here's a Single-Part Miscellaneous Joke:";
             joke.innerText = data.joke;
         }
     })
+    .catch(error => console.error(error));
 }
 
 function getSpooky() {
-    fetch("https://v2.jokeapi.dev/joke/Spooky?safe-mode")  //Fetch a pun joke
+    fetch("https://v2.jokeapi.dev/joke/Spooky?safe-mode")  //Fetch a spooky joke
     .then(response => response.json())
     .then(data => {
-            console.log(data);
-
-            jokeType.innerText = "Joke type: " + data.type;
+        console.log(data);
+        jokeType.innerText = "Joke type: " + data.type;
 
 
         pageTitle.innerText = "A Random Spooky Joke"; 
 
         if (data.type == "twopart" ){
             //Print the type of joke
-            jokeType.innerText = "Here's a two-part spooky joke:";
-            joke.innerHTML = data.setup + " ... " + data.delivery;
+            jokeType.innerText = "Here's a Two-Part Spooky Joke:";
+            joke.innerText = data.setup + " ... " + data.delivery;
         }
 
         else{
-            jokeType.innerText = "Here's a single-part spooky joke:";
+            jokeType.innerText = "Here's a Single-Part Spooky Joke:";
             joke.innerText = data.joke;
         }
     })
+    .catch(error => console.error(error));
 }
 
 function getChristmas() {
-    fetch("https://v2.jokeapi.dev/joke/Christmas?safe-mode")  //Fetch a pun joke
+    fetch("https://v2.jokeapi.dev/joke/Christmas?safe-mode")  //Fetch a christmas joke
     .then(response => response.json())
     .then(data => {
-            console.log(data);
-
-            jokeType.innerText = "Joke type: " + data.type;
-
+        console.log(data);
+        jokeType.innerText = "Joke type: " + data.type;
         pageTitle.innerText = "A Random Christmas Joke"; 
 
         if (data.type == "twopart" ){
             //Print the type of joke
-            jokeType.innerText = "Here's a two-part Christmas joke:";
-            joke.innerHTML = data.setup + " ... " + data.delivery;
+            jokeType.innerText = "Here's a Two-Part Christmas Joke:";
+            joke.innerText = data.setup + " ... " + data.delivery;
         }
 
         else{
-            jokeType.innerText = "Here's a single-part Christmas joke:";
+            jokeType.innerText = "Here's a Single-Part Christmas Joke:";
             joke.innerText = data.joke;
         }
     })
+    .catch(error => console.error(error));
 }
 
 function getProgram() {
-    fetch("https://v2.jokeapi.dev/joke/Programming?safe-mode")  //Fetch a pun joke
+    fetch("https://v2.jokeapi.dev/joke/Programming?safe-mode")  //Fetch a programming joke
     .then(response => response.json())
     .then(data => {
-            console.log(data);
+        console.log(data);
 
-            jokeType.innerText = "Joke type: " + data.type;
-            
-
+        jokeType.innerText = "Joke type: " + data.type;
         pageTitle.innerText = "A Random Programming Joke"; 
 
         if (data.type == "twopart" ){
             //Print the type of joke
-            jokeType.innerText = "Here's a two-part programming joke:";
-            joke.innerHTML = data.setup + " ... " + data.delivery;
+            jokeType.innerText = "Here's a Two-Part Programming Joke:";
+            joke.innerText = data.setup + " ... " + data.delivery;
         }
 
         else{
-            jokeType.innerText = "Here's a single-part programming joke:";
+            jokeType.innerText = "Here's a Single-Part Programming Joke:";
             joke.innerText = data.joke;
         }
     })
+    .catch(error => console.error(error));
 }
+
+//WHEN THE PAGE FIRST LOADS,  DO THE JOKE OF THE DAY.
+window.onload = jokeOfDay;
+
+//Event Listeners
+
+buttonPun.addEventListener("click", getPun);
+buttonMisc.addEventListener("click", getMisc);
+buttonSpooky.addEventListener("click", getSpooky);
+buttonChristmas.addEventListener("click", getChristmas);
+buttonProgram.addEventListener("click", getProgram);
+
+
