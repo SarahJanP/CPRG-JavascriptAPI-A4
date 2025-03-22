2ca1c1e487a7c93f5c09edc5145b845067f54625
SarahJanP <sarah.parker@edu.sait.ca>
2025-03-22 14:47:32 -0600

    Feat: Added all of the buttons, their functions, changing page title, and joke of the day on load.  Final Tuning is all thats left.

--- scripts.js
+++ scripts.js
@@ -1,32 +1,167 @@
-//make sure u use SAFE jokes 
-
-//const initialization for necessary elements.
-const allData = document.getElementById("allData");
+// Sarah Parker | 000900287
 const pageTitle = document.getElementById("pageTitle");
-const jokeType = document.getElementById("jokeType");
-const jokeSetup = document.getElementById("jokeSetup");
-const jokeDelivery = document.getElementById("jokeDelivery");
-const twoPartBool = document.getElementById("twoPartBool");
 const jokeCategory = document .getElementById("jokeCategory")
+const jokeSection = document.getElementById("jokeSection");
 
-fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
+
+//WHEN THE PAGE FIRST LOADS,  DO THE JOKE OF THE DAY.
+window.onload = jokeOfDay();
+
+//Button Constants
+const randomPun = document.getElementById("buttonPun")
+const randomMisc = document.getElementById("buttonMisc")
+const randomSpooky = document.getElementById("buttonSpooky")
+const randomChristmas = document.getElementById("buttonChristmas")
+const randomProgram = document.getElementById("buttonProgram")
+
+//Event Listeners
+
+randomPun.addEventListener("click", getPun);
+randomMisc.addEventListener("click", getMisc);
+randomSpooky.addEventListener("click", getSpooky);
+randomChristmas.addEventListener("click", getChristmas);
+randomProgram.addEventListener("click", getProgram);
+
+        
+//FUNCTIONS
+
+function jokeOfDay(){
+    fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        pageTitle.innerText = "Joke Of The Day!"
+
+
+        if (data.type == "twopart" ){
+            //Print the type of joke
+            jokeType.innerText = "Here's a two-part joke:";
+            joke.innerHTML = data.setup + " ... " + data.delivery;
+        }
+
+        else{
+            jokeType.innerText = "Here's a single-part joke:";
+            joke.innerText = data.joke;
+        }
+    });
+}
+
+
+
+function getPun() {
+    fetch("https://v2.jokeapi.dev/joke/Pun?safe-mode")  //Fetch a pun joke
     .then(response => response.json())
     .then(data => {
             console.log(data);
-                //Print the type of joke
             jokeType.innerText = "Joke type: " + data.type;
 
-                //Print the joke category
-            jokeCategory.innerText = "Joke category: " + data.category;
+
+
+        pageTitle.innerText = "A Random Pun"; 
+
         if (data.type == "twopart" ){
-            jokeSetup.innerText = data.setup;
-            twoPartBool.innerHTML = "<p> ... </p>"
-            jokeDelivery.innerText=data.delivery;
+            //Print the type of joke
+            jokeType.innerText = "Here's a two-part pun:";
+            joke.innerHTML = data.setup + " ... " + data.delivery;
         }
 
-        if (data.type == "single"){
-            jokeSetup.innerText = data.joke;
+        else{
+            jokeType.innerText = "Here's a single-part pun:";
+            joke.innerText = data.joke;
         }
-    
-});
+    })
+}
 
+
+function getMisc() {
+    fetch("https://v2.jokeapi.dev/joke/Misc?safe-mode")  //Fetch a pun joke
+    .then(response => response.json())
+    .then(data => {
+            console.log(data);
+            jokeType.innerText = "Joke type: " + data.type;
+
+        pageTitle.innerText = "A Random Miscellaneous Joke"; 
+
+        if (data.type == "twopart" ){
+            //Print the type of joke
+            jokeType.innerText = "Here's a two-part miscellaneous joke:";
+            joke.innerHTML = data.setup + " ... " + data.delivery;
+        }
+
+        else{
+            jokeType.innerText = "Here's a single-part miscellaneous joke:";
+            joke.innerText = data.joke;
+        }
+    })
+}
+
+function getSpooky() {
+    fetch("https://v2.jokeapi.dev/joke/Spooky?safe-mode")  //Fetch a pun joke
+    .then(response => response.json())
+    .then(data => {
+            console.log(data);
+
+            jokeType.innerText = "Joke type: " + data.type;
+
+
+        pageTitle.innerText = "A Random Spooky Joke"; 
+
+        if (data.type == "twopart" ){
+            //Print the type of joke
+            jokeType.innerText = "Here's a two-part spooky joke:";
+            joke.innerHTML = data.setup + " ... " + data.delivery;
+        }
+
+        else{
+            jokeType.innerText = "Here's a single-part spooky joke:";
+            joke.innerText = data.joke;
+        }
+    })
+}
+
+function getChristmas() {
+    fetch("https://v2.jokeapi.dev/joke/Christmas?safe-mode")  //Fetch a pun joke
+    .then(response => response.json())
+    .then(data => {
+            console.log(data);
+
+            jokeType.innerText = "Joke type: " + data.type;
+
+        pageTitle.innerText = "A Random Christmas Joke"; 
+
+        if (data.type == "twopart" ){
+            //Print the type of joke
+            jokeType.innerText = "Here's a two-part Christmas joke:";
+            joke.innerHTML = data.setup + " ... " + data.delivery;
+        }
+
+        else{
+            jokeType.innerText = "Here's a single-part Christmas joke:";
+            joke.innerText = data.joke;
+        }
+    })
+}
+
+function getProgram() {
+    fetch("https://v2.jokeapi.dev/joke/Programming?safe-mode")  //Fetch a pun joke
+    .then(response => response.json())
+    .then(data => {
+            console.log(data);
+
+            jokeType.innerText = "Joke type: " + data.type;
+            
+
+        pageTitle.innerText = "A Random Programming Joke"; 
+
+        if (data.type == "twopart" ){
+            //Print the type of joke
+            jokeType.innerText = "Here's a two-part programming joke:";
+            joke.innerHTML = data.setup + " ... " + data.delivery;
+        }
+
+        else{
+            jokeType.innerText = "Here's a single-part programming joke:";
+            joke.innerText = data.joke;
+        }
+    })
+}
